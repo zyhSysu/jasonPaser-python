e7540c6bfbccc22472a39b4d9b148a5bbb0c19d5
zyhSysu <zhyhuan@mail2.sysu.edu.cn>
2016-06-19 21:49:07 +0800

    Complete Version 1
    Implement all important functions but without testing them

--- exceptionHandler.py
+++ exceptionHandler.py
@@ -6,23 +6,31 @@ PARSER_SUCCESS = 0
 PARSER_EMPTYDICT = 1
 PARSER_SYNTAXERROR = 2
 PARSER_EMPTYJASON = 3
+PARSER_NUMERICERROR = 4
 
 class StatusHandler:
 
 	def status_success(self):
 
-		print "JasonPaser Success"
+		print "JasonParser Success"
 
 
 	def status_emptyDict(self):
 
-		print "JasonPaser Exception: EmptyDict Exception"
+		print "JasonParser Exception: EmptyDict Exception"
 
 
 	def status_syntaxError(self):
 
-		print "JasonPaser Exception: SyntaxError Exception"
+		print "JasonParser Exception: SyntaxError Exception"
 
+	def status_emptyJason(self):
+
+		print "JasonParser Exception: EmptyJason Exception"
+
+	def status_numericError(self):
+
+		print "JasonParser Exception: InvalidNumber Exception"
 
 	def displayStatus(self, code):
 
@@ -30,6 +38,8 @@ class StatusHandler:
 		0: self.status_success,
 		1: self.status_emptyDict,
 		2: self.status_syntaxError
+		3: self.status_emptyJason
+		4: self.status_numericError
 		}
 
 		statusCase[code]()
--- parser.py
+++ parser.py
@@ -7,6 +7,7 @@ PARSER_SUCCESS = 0
 PARSER_EMPTYDICT = 1
 PARSER_SYNTAXERROR = 2
 PARSER_EMPTYJASON = 3
+PARSER_NUMERICERROR = 4
 
 
 class JasonParser:
@@ -19,18 +20,14 @@ class JasonParser:
 
 		jasonStr = self.deleteSpace(jasonStr, begin)
 
-		elif len(jasonStr) == 0:
-
+		if len(jasonStr) == 0:
 			return PARSER_EMPTYJASON
 		elif len(jasonStr) < 2:
-
 			return PARSER_SYNTAXERROR
 		elif jasonStr[0] != '{' or jasonStr[-1] != '}':
-
 			return PARSER_SYNTAXERROR
 		else:
-
-			self.jasonDict, status = self.parseObject(str(jasonStr)[1: -1])
+			self.jasonDict, step, status = self.parseObject(str(jasonStr)[0: len(jasonStr)])
 
 			return status
 
@@ -39,51 +36,191 @@ class JasonParser:
 		
 		jasonObj = dict()
 
-		#Begin to parse the jason object
+		#check the braces completion
+		twoBraces = False
+
+		#begin to parse the jason object
 		index = 0
 
+		if jasonStr[index] != '{':
+			return None, -1, PARSER_SYNTAXERROR
+
+		index += 1
+
 		while index < len(jasonStr):
+
+			if jasonStr[index] == '}':
+				twoBraces = True
+
+				break
+
 			#parse key(string)
-			keyHead, keyTail = self.parseString(jasonStr)
+			strVlue, step, status = self.parseString(jasonStr[index: len(jasonStr)])
 
-			key = jasonStr[keyHead:keyTail]
+			if status != PARSER_SUCCESS:
+				return None, -1, PARSER_SYNTAXERROR
 
-			index = keyTail+1
+			key = strVlue
 
+			index += step
+
+			#parse symbol ":"
 			if index >= len(jasonStr) or jasonStr[index] != ':':
-
 				return None, PARSER_SYNTAXERROR
+
+			#parse value
+			index += 1
+
+			if jasonStr[index] == '"':							#value is string
+				strValue, step, status = self.parseString(jasonStr[index: len(jasonStr)])
+
+				if status != PARSER_SUCCESS:
+					return None, -1, PARSER_SYNTAXERROR
+
+				jasonObj[key] = strValue
+
+				index += step
+			elif jasonStr[index].isdigit() == True:				#value is number
+				numValue, step, status = self.parseNumber(jasonStr[index: len(jasonStr)])
+
+				if status != PARSER_SUCCESS:
+					return None, -1, PARSER_SYNTAXERROR
+
+				jasonObj[key] = numValue
+
+				index += step
+			elif jasonStr[index] == '[':						#value is array
+				arrayValue, step, status = self.parseArray(jasonStr[index: len(jasonStr)])
+
+				if status != PARSER_SUCCESS:
+					return None, -1, PARSER_SYNTAXERROR
+
+				jasonObj[key] = arrayValue
+
+				index += step
+			elif jasonStr[index] == '{':						#value is obj
+				objValue, step, status = self.parseObject(jasonStr[index: len(jasonStr)])
+
+				if status != PARSER_SUCCESS:
+					return None, -1, PARSER_SYNTAXERROR
+
+				jasonObj[key] = objValue
+
+				index += step
 			else:
+				return None, -1, PARSER_SYNTAXERROR
+
+			if index < len(jasonStr) and jasonStr[index] == ',':
 				index += 1
 
-				if jasonStr[index] == '"':		#value is string
-					strHead, strTail = self.parseString(jasonStr[index, len(jasonStr)])
-
-					jasonObj[key] = jasonStr[strHead:strTail]
-
-					index = strTail
-				elif jasonStr[index] == '': #regular expression:	#value is numeric
-				
-					pass
-				elif jasonStr[index] == '[':						#value is array
-
-					pass
-				elif jasonStr[index] == '{':						#value is obj
-
-					pass
-				else:
-					return PARSER_SYNTAXERROR
-
-		return jasonObj, PARSER_SUCCESS
+		if twoBraces == True:
+			return jasonObj, index+1, PARSER_SUCCESS
+		else:
+			return None, -1, PARSER_SYNTAXERROR
 
 	def parseArray(self, jasonStr):
 
-		pass
+		jasonArray = list()
+
+		#check the braces completion
+		twoBraces = False
+
+		#begin to parse jason array
+		index = 0
+
+		if jasonStr[index] != '[':
+			return None, -1, PARSER_SYNTAXERROR
+
+		index += 1
+
+		while index < len(jasonStr):
+			if jasonStr[index] == ']':
+				twoBraces = True
+
+				break
+
+			if jasonStr[index] == '"':						#element is string
+				strValue, step, status = self.parseString(jasonStr[index: len(jasonStr)])
+
+				if status != PARSER_SUCCESS:
+					return None, -1, PARSER_SYNTAXERROR
+
+				jasonArray.append(strValue)
+
+				index += step
+			elif jasonStr[index].isdigit() == True:			#element is number
+				numValue, step, status = self.parseNumber(jasonStr[index: len(jasonStr)])
+
+				if status != PARSER_SUCCESS:
+					return None, -1, PARSER_SYNTAXERROR
+
+				jasonArray.append(numValue)
+
+				index += step
+			else:
+				return None, -1, PARSER_SYNTAXERROR
+
+		if twoBraces == True:
+			return jasonArray, index+1, PARSER_SUCCESS
+		else:
+			return None, -1, PARSER_SYNTAXERROR
+
 
 	def parseString(self, jasonStr):
 
-		pass
+		#check the quotation marks completion
+		twoQuotation = False
 
+		#begin to parse string
+		index = 0
+
+		if jasonStr[index] != '"':
+			return None, -1, PARSER_SYNTAXERROR
+
+		index += 1
+
+		while index < len(jasonStr):
+			if jasonStr[index] == '"':
+				twoQuotation = True
+
+				break
+			
+			index += 1
+
+
+		if twoQuotation == True:
+			if index > 1:
+				return jasonStr[1: index], index+1, PARSER_SUCCESS
+			else:												#empty string element
+				return "", index+1, PARSER_SUCCESS
+		else:
+			return None, -1, PARSER_SYNTAXERROR
+
+	def parseNumber(self, jasonStr):
+
+		#whether the number is double
+		isFloat = False
+
+		#begin to parse number
+		index = 0
+
+		while index < len(jasonStr):
+			if jasonStr[index].isdigit() == False:
+				break
+			elif jasonStr[index] == '.':
+				if isFloat == False:
+					isFloat = True
+				else:
+					return None, -1, PARSER_NUMERICERROR
+
+			index += 1
+
+		if isFloat == True:
+			return float(jasonStr[0: index]), index, PARSER_SUCCESS
+		else:
+			return int(jasonStr[0: index]), index, PARSER_SUCCESS
+
+	#use regular expression to eliminate all spaces
 	def deleteSpace(self, jasonStr):
 
 		space = re.compile(r"[\r\n\s\t]")
@@ -95,7 +232,7 @@ class JasonParser:
 		if len(self.jasonDict) == 0:
 
 			return PARSER_EMPTYDICT
-		else:
+		else:									#print dictionary
 
 			pass
 
