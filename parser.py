ca6dcd8f728e95d0c20f3f823825d04ddc09cc90
zyhSysu <zhyhuan@mail2.sysu.edu.cn>
2016-06-19 09:45:23 +0800

    Modify function deleteSpace
    Use regular expression instead of brute force comparison to eliminate
    the space

--- parser.py
+++ parser.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import sys
+import re
 
 PARSER_SUCCESS = 0
 PARSER_EMPTYDICT = 1
@@ -16,9 +17,9 @@ class JasonParser:
 
 	def parseJason(self, jasonStr):
 
-		jasonStr = self.deleteSpace(jasonStr)
+		jasonStr = self.deleteSpace(jasonStr, begin)
 
-		if len(jasonStr) == 0:
+		elif len(jasonStr) == 0:
 
 			return PARSER_EMPTYJASON
 		elif len(jasonStr) < 2:
@@ -29,53 +30,65 @@ class JasonParser:
 			return PARSER_SYNTAXERROR
 		else:
 
-			self.jasonDict = self.parseObject(str(jasonStr)[1: -1])
+			self.jasonDict, status = self.parseObject(str(jasonStr)[1: -1])
 
-			return PARSER_SUCCESS
+			return status
 
 
 	def parseObject(self, jasonStr):
 		
 		jasonObj = dict()
 
-		jasonStr = self.deleteSpace(jasonStr)
+		#Begin to parse the jason object
+		index = 0
 
-		#Begin to parse the jason
+		while index < len(jasonStr):
+			#parse key(string)
+			keyHead, keyTail = self.parseString(jasonStr)
 
-		return jasonObj
+			key = jasonStr[keyHead:keyTail]
+
+			index = keyTail+1
+
+			if index >= len(jasonStr) or jasonStr[index] != ':':
+
+				return None, PARSER_SYNTAXERROR
+			else:
+				index += 1
+
+				if jasonStr[index] == '"':		#value is string
+					strHead, strTail = self.parseString(jasonStr[index, len(jasonStr)])
+
+					jasonObj[key] = jasonStr[strHead:strTail]
+
+					index = strTail
+				elif jasonStr[index] == '': #regular expression:	#value is numeric
+				
+					pass
+				elif jasonStr[index] == '[':						#value is array
+
+					pass
+				elif jasonStr[index] == '{':						#value is obj
+
+					pass
+				else:
+					return PARSER_SYNTAXERROR
+
+		return jasonObj, PARSER_SUCCESS
 
 	def parseArray(self, jasonStr):
 
 		pass
 
+	def parseString(self, jasonStr):
+
+		pass
+
 	def deleteSpace(self, jasonStr):
 
-		if len(jasonStr) > 0:
-			head, tail = 0, len(jasonStr)-1
+		space = re.compile(r"[\r\n\s\t]")
 
-			#eliminate space in the head
-			while head <= tail:
-				if jasonStr[head] == ' ' or jasonStr[head] == '\r' \
-					or jasonStr[head] == '\n':
-
-					head += 1
-				else:
-					break
-
-			#eliminate space in the tail
-			while tail >= head:
-				if jasonStr[tail] == ' ' or jasonStr[tail] == '\r' \
-					or jasonStr[tail] == '\n':
-
-					tail -= 1
-				else:
-					break
-
-
-			return jasonStr[head: tail+1]
-
-		else:
-			return jasonStr
+		return re.sub(space, '', jasonStr)
 
 	def printJasonDict(self):
 
